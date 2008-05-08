eb61e3131ad6f2cf3eedb5b588726902700f87f6
James Adam <james@lazyatom.com>
2008-05-08 16:08:22 +0100

    Ensure we dont try to sort by nil.

--- lib/vanilla/dynasnips/kind.rb
+++ lib/vanilla/dynasnips/kind.rb
@@ -2,7 +2,7 @@ require 'vanilla/dynasnip'
 
 class Kind < Dynasnip
   def handle(kind, limit=10)
-    Soup.sieve(:kind => kind)[0...limit].sort_by { |s| s.created_at }.reverse.map do |snip|
+    Soup.sieve(:kind => kind)[0...limit].sort_by { |s| s.created_at || 0 }.reverse.map do |snip|
       snip_template.gsub('SNIP', app.render(snip))
     end
   end
