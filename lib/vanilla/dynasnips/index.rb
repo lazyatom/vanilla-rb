77f469c85e6b43aea15b3f1740c7d56164471a25
James Adam <james@lazyatom.com>
2008-06-02 00:23:24 +0100

    Fix the routes in the index dyna.

--- lib/vanilla/dynasnips/index.rb
+++ lib/vanilla/dynasnips/index.rb
@@ -4,7 +4,7 @@ class Index < Dynasnip
   def get(*args)
     # TODO: figure out a way around calling Soup/AR methods directly.
     list = Soup.tuple_class.find_all_by_name('name').map { |tuple| 
-      "<li>#{Routes.link_to tuple.value}</li>"
+      "<li>#{Vanilla::Routes.link_to tuple.value}</li>"
     }
     "<ol>#{list}</ol>"
   end
