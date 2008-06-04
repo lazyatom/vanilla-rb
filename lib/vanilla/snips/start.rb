d7eed03ee3dd080c0097c51267d90c85238bb706
James Adam <james@lazyatom.com>
2008-06-04 23:13:34 +0100

    Include the tutorial snip in the start snip - it's a nicer introduction.

--- lib/vanilla/snips/start.rb
+++ lib/vanilla/snips/start.rb
@@ -1,14 +1,16 @@
 start = Snip.new(:name => 'start')
 start.content = <<-START
-This is the start snip, which is the default home page.
+This is the {link_to start} snip, which is the default home page.
 
-You might want to check out the {link_to test} snip. In fact - I'll include it here for you.
+You might want to check out the {link_to vanilla-rb-tutorial} snip. 
+
+In fact - I'll include it here for you:
+
+{vanilla-rb-tutorial}
 
 ---
 
-{test}
-
----
+That was the end of the tutorial snip. We're back in the start snip now. There's also the {link_to test} snip, that might be interesting.
 
 Anyway, welcome.
 START
