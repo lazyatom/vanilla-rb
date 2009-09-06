edf0bb6a9d0a84cbedc115d66efdbb4cf24d4b2d
James Adam <james@lazyatom.com>
2009-09-06 09:55:28 +0100

    Display the index with most recent first.

--- lib/vanilla/dynasnips/index.rb
+++ lib/vanilla/dynasnips/index.rb
@@ -3,7 +3,7 @@ require 'vanilla/dynasnip'
 class Index < Dynasnip
   def get(*args)
     # TODO: figure out a way around calling Soup/AR methods directly.
-    list = Soup.tuple_class.find_all_by_name('name').map { |tuple| 
+    list = Soup.tuple_class.find_all_by_name('name', :order => 'updated_at DESC').map { |tuple| 
       "<li>#{Vanilla::Routes.link_to tuple.value}</li>"
     }
     "<ol>#{list}</ol>"
