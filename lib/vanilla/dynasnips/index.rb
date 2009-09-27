1b72b90d66e890ec9e412ded8c48fb8be9f2c51c
James Adam <james@lazyatom.com>
2009-09-27 20:44:06 +0100

    Fixed a bug rendering the index of all snips.

--- lib/vanilla/dynasnips/index.rb
+++ lib/vanilla/dynasnips/index.rb
@@ -2,7 +2,7 @@ require 'vanilla/dynasnip'
 
 class Index < Dynasnip
   def get(*args)
-    list = app.soup.all_snips.sort { |a,b| a.updated_at > b.updated_at }.map { |snip|
+    list = app.soup.all_snips.sort_by { |a| a.updated_at }.reverse.map { |snip|
       "<li>#{link_to snip.name}</li>"
     }
     "<ol>#{list}</ol>"
