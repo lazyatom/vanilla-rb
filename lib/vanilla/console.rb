d8649ee3bc98fe9fee550fc37c5282be538797f7
James Adam <james@lazyatom.com>
2009-09-27 20:43:17 +0100

    Actually make the console work.

--- lib/vanilla/console.rb
+++ lib/vanilla/console.rb
@@ -1,4 +1,9 @@
 require 'vanilla'
-app = Vanilla::App.new(ENV['VANILLA_CONFIG'])
-soup = app.soup
+require 'irb'
+
+def app(reload=false)
+  @__vanilla_console_app = nil if reload
+  @__vanilla_console_app ||= Vanilla::App.new(ENV['VANILLA_CONFIG'])
+end
+
 puts "The Soup is simmering."
