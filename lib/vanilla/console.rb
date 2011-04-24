a1617b9b6f42abf2f23c69c6bce3325d94a2418d
James Adam <james@lazyatom.com>
2011-04-24 22:18:45 +0100

    This is a bit tidier.

--- lib/vanilla/console.rb
+++ lib/vanilla/console.rb
@@ -2,9 +2,11 @@ require 'vanilla'
 require 'irb'
 
 def app(reload=false)
-  @__vanilla_console_app = nil if reload
-  config = YAML.parse_file(ENV['VANILLA_CONFIG']) rescue {}
-  @__vanilla_console_app ||= Vanilla::App.new(config)
+  if !@__vanilla_console_app || reload
+    config = YAML.parse_file(ENV['VANILLA_CONFIG']) rescue {}
+    @__vanilla_console_app = Vanilla::App.new(config)
+  end
+  @__vanilla_console_app
 end
 
 puts "The Soup is simmering."
