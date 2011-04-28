1f03ce9050ad6c26dfef32ec632794854ca58a8e
James Adam <james@lazyatom.com>
2011-04-28 23:19:01 +0100

    Escape HTML-sensitive characters in debug output.

--- pristine_app/soups/dynasnips/debug.rb
+++ pristine_app/soups/dynasnips/debug.rb
@@ -1,14 +1,15 @@
 require 'vanilla/dynasnip'
+require 'cgi'
 
 # If the dynasnip is a subclass of Dynasnip, it has access to the request hash
 # (or whatever - access to some object outside of the snip itself.)
 class Debug < Dynasnip
   def get(*args)
-    app.request.inspect
+    CGI.escapeHTML(app.request.inspect)
   end
 
   def post(*args)
-    "You posted! " + app.request.inspect
+    "You posted! " + CGI.escapeHTML(app.request.inspect)
   end
   self
 end
