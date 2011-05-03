7b82b94b9178202c01443508b4a51b7cafb8ced7
James Adam <james@lazyatom.com>
2011-05-03 11:46:04 +0100

    Tidy up unused/unnecessary routing methods to make the link_to tests pass.

--- lib/vanilla/routes.rb
+++ lib/vanilla/routes.rb
@@ -2,30 +2,17 @@ module Vanilla
   # Expects to be able to call 'soup' on whatever it is included into
   module Routes
     def link_to(link_text, snip_name=link_text, part=nil)
-      soup[snip_name] ? existing_link(link_text, snip_name, part) : new_link(snip_name)
+      if soup[snip_name]
+        %{<a href="#{url_to(snip_name, part)}">#{link_text}</a>}
+      else
+        %{<a class="missing" href="#{url_to(snip_name, part)}">#{link_text}</a>}
+      end
     end
 
     def url_to(snip_name, part=nil)
       url = "/#{snip_name.gsub(" ", "+")}"
       url += "/#{part}" if part
       url
-    end
-
-    def url_to_raw(snip_name, part=nil)
-      url = url_to(snip_name, part)
-      url += ".raw"
-    end
-
-    def existing_link(link_text, snip_name=link_text, part=nil)
-      %{<a href="#{url_to(snip_name, part)}">#{link_text}</a>}
-    end
-
-    def edit_link(snip_name, link_text)
-      %[<a href="/edit?name=#{snip_name.gsub(" ", "+")}">#{link_text}</a>]
-    end
-
-    def new_link(snip_name="New")
-      %[<a href="/new?name=#{snip_name ? snip_name.gsub(" ", "+") : nil}" class="new">#{snip_name}</a>]
     end
   end
 end
