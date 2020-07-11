a21baf879ae1896fe3ee9cba9575162d611e3330
James Adam <james@lazyatom.com>
2020-07-11 18:24:02 +0100

    Ignore snip references in markdown code blocks
    It's extremely unlikely that we'd want to reference snips in here, and
    far more likely that we'd actually have curly braces we want to ignore.

--- lib/vanilla/renderers/markdown.rb
+++ lib/vanilla/renderers/markdown.rb
@@ -4,7 +4,10 @@ require 'bluecloth'
 module Vanilla::Renderers
   class Markdown < Base
     def process_text(content)
-      BlueCloth.new(content).to_html
+      html = BlueCloth.new(content).to_html
+      html.gsub(/<pre>(.*)<\/pre>/m) do |match|
+        self.class.escape_curly_braces(match)
+      end
     end
   end
 end
--- test/core/renderers/markdown_renderer_test.rb
+++ test/core/renderers/markdown_renderer_test.rb
@@ -23,6 +23,17 @@ Markdown
       create_snip(:name => "another_snip", :content => "blah", :render_as => "Bold")
       assert_response_body "<h1>markdown</h1>\n\n<p>and so lets include <b>blah</b></p>", "/test"
     end
+
+    should 'ignore snip inclusions in code-indented blocks' do
+      content = <<Markdown
+Content
+
+    Some {indented} code
+Markdown
+      markdown_snip(name: 'test', content: content)
+      expected_code = %|<p>Content</p>\n\n<pre><code>Some &#123;indented&#125; code\n</code></pre>|
+      assert_response_body expected_code, '/test'
+    end
   end
 
   def markdown_snip(attributes)
